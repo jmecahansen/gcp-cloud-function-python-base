509ad7cdc2a5dcf392da39f204107a0a838edfd6
jmecahansen <jmecahansen@gmail.com>
2023-08-14 13:58:23 +0200

    Added application defaults

---
+++ src/defaults.py
@@ -0,0 +1,9 @@
+"""
+application defaults
+"""
+
+# aliases and/or imports
+from os.path import abspath, dirname, relpath
+
+# root application path
+root_path = abspath(relpath(f"{dirname(__file__)}/.."))
