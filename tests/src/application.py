eec77ac8499727cfe25b9894ae028495a34ccac2
jmecahansen <jmecahansen@gmail.com>
2023-08-17 14:09:06 +0200

    Added basic unit test (application)

---
+++ tests/src/application.py
@@ -0,0 +1,29 @@
+"""
+application
+"""
+
+# aliases and/or imports
+from flask import Request, Response
+from src.application import ApplicationClass
+import pytest
+
+# create an empty request
+empty_request = Request({})
+
+
+@pytest.mark.parametrize("arguments, expectation ", [
+    ({}, b"{\"status\":\"OK\"}"),
+])
+def test_response_matches_expectation(arguments: dict,
+                                      expectation: str):
+    """
+    test the response matches its expectation
+    :param arguments: the HTTP request arguments
+    :param expectation: the expected JSON-encoded output
+    """
+
+    response = ApplicationClass(Request(arguments)).run()
+    assert (
+        isinstance(response, Response)
+        and response.data == expectation
+    )
