930bcfa7340782f113055d10f6baff7252b1e7d7
jmecahansen <jmecahansen@gmail.com>
2023-08-17 12:00:00 +0200

    Added default (JSON encoding options)

--- src/defaults.py
+++ src/defaults.py
@@ -5,5 +5,11 @@ application defaults
 # aliases and/or imports
 from os.path import abspath, dirname, relpath
 
+# JSON encoding options
+json_encoding_options = {
+    "ensure_ascii": False,
+    "separators": (":", ",")
+}
+
 # root application path
 root_path = abspath(relpath(f"{dirname(__file__)}/.."))
