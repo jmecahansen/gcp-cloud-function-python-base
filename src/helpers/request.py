1645eb53505a8da8568a0f9dc965a2c4fbf3f40b
jmecahansen <jmecahansen@gmail.com>
2023-11-06 10:18:03 +0100

    Added get_data()

--- src/helpers/request.py
+++ src/helpers/request.py
@@ -3,9 +3,10 @@ request
 """
 
 # aliases and/or imports
-from flask import Response
+from flask import Request, Response
 from json import dumps
 from src.defaults import json_encoding_options
+from typing import Any
 
 
 def build_response(code: int,
@@ -28,3 +29,38 @@ def build_response(code: int,
     response.mimetype = "application/json"
     response.status_code = code
     return response
+
+
+def get_data(request: Request,
+             key: str,
+             fallback: Any = None) -> Any:
+    """
+    return the value of a specific variable
+    :param request: the request object
+    :param key: the variable key
+    :param fallback: the fallback value (optional)
+    :return: the value
+    """
+
+    if not request:
+        raise Exception("no request data")
+
+    if (
+        request.args
+        and key in request.args
+    ):
+        return request.args.get(key)
+
+    if (
+        request.form
+        and key in request.form
+    ):
+        return request.form.get(key)
+
+    if request.json:
+        data = request.get_json(silent=True)
+
+        if key in data:
+            return data[key]
+
+    return fallback
